92caa625b0c0b884ece3403edeb1d6815fec1e5a
Fabio Cruz <fabio.cruz@etus.group>
2021-01-06 22:13:35 -0300

    Preparing repository package

---
+++ app/Http/Repositories/AbstractRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+abstract class AbstractRepository {
+
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = $this->resolveModel();
+    }
+
+    protected function resolveModel()
+    {
+        return app($this->model);
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+}
